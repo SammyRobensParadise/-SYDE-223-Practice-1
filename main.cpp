8310f08899002a642448d376126c7627d255900b
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-01-09 16:58:48 -0500

    added get_artist class

--- main.cpp
+++ main.cpp
@@ -4,20 +4,32 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+
 using namespace std;
-class Music{
+
+class Music {
 
 private:
-   unsigned int dateMade;
-   string artistName;
-   string musicID;
+    unsigned int dateMade;
+    string artistName;
+    string musicID;
 public:
     Music(int dateMade = 0);
-    Music(int dateMade,string artistName,string musicID);
-    string get_artist(){
+
+    Music(int dateMade, string artistName, string musicID);
+
+    string get_artist() {
         return artistName;
     }
+
+    friend bool operator==(const Music &comp1, const Music &comp2);
 };
+
+bool operator==(const Music &comp1, const Music &comp2) {
+    return comp1.dateMade == comp2.dateMade &&
+           comp1.artistName == comp2.artistName
+           && comp1.musicID == comp2.musicID;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
