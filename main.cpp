55eeddd3cfbfc4abd8db73ae9432f7ce673e4af3
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-01-09 16:20:38 -0500

    iniit class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+class Music{
+
+private:
+   unsigned int dateMade;
+   string artistName;
+   string musicID;
+public:
+    Music();
+    Music(int dateMade,string artistName,string musicID);
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
