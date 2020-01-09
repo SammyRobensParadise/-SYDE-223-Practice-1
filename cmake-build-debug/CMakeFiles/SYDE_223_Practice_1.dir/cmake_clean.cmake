f35b6583bc59c231cda47cf235aa50423974be85
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-01-09 16:05:26 -0500

    initialized repo

---
+++ cmake-build-debug/CMakeFiles/SYDE_223_Practice_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SYDE_223_Practice_1.dir/main.cpp.o"
+  "SYDE_223_Practice_1.pdb"
+  "SYDE_223_Practice_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SYDE_223_Practice_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
