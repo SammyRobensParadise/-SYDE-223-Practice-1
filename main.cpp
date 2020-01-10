a66aaa3e501f5994099e28110873af2c01cd1032
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-01-09 21:27:55 -0500

    added doc strings

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/**
+ * @author Sammy Robens-Paradise
+ * @author Mary McPhee
+ * @date Friday Janurary 10, 2020
+ */
+
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -6,7 +12,10 @@
 #include <math.h>
 #include <algorithm>
 
+/** @namespace {std} */
 using namespace std;
+
+// constants
 const int MAX_NUMBER_OF_UNIQUE_ARTISTS = 3;
 
 class Music {
@@ -16,14 +25,30 @@ private:
     string artistName;
     string musicID;
 public:
+    /**
+     *
+     * @param {int} dateMade
+     */
     Music(int dateMade = 0);
-
+    /**
+     *
+     * @param {int} dateMade
+     * @param {string} artistName
+     * @param {string} musicID
+     */
     Music(int dateMade, string artistName, string musicID);
-
+    /**
+     *
+     * @return {string} artistName
+     */
     string get_artist() {
         return artistName;
     }
-
+    /**
+     *
+     * @param {Music} comp2
+     * @return boolean
+     */
     bool operator==(const Music &comp2) {
         return dateMade == comp2.dateMade &&
                artistName == comp2.artistName
@@ -38,10 +63,23 @@ private:
     string songName;
 
 public:
+    /**
+     *
+     * @param {int} songLength
+     */
     Song(int songLength = 0);
-
+    /**
+     *
+     * @param {int} songLength
+     * @param {string} genre
+     * @param {string} songName
+     */
     Song(int songLength, string genre, string songName);
-
+    /**
+     *
+     * @param {Song} comp2
+     * @return
+     */
     bool operator==(const Song &comp2) {
         return songLength == comp2.songLength &&
                genre == comp2.genre &&
@@ -54,7 +92,15 @@ class Playlist {
 private:
     vector<Song> my_playlist;
 public:
-    Playlist()
+    /**
+     * constructor
+     */
+    Playlist();
+    /**
+     * insert_songs
+     * @param {Song} songToInsert
+     * @return bool
+     */
     bool insert_songs(const Song &songToInsert) {
         string artistName = static_cast<Music>(songToInsert).get_artist();
         int unique_artist_count = 0;
@@ -71,10 +117,15 @@ public:
     };
 
     Playlist shuffleSongs(){
+        // TODO implement shuffle songs
+
     }
 };
 
-
+/**a
+ * @name main
+ * @return {int} 0
+ */
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
